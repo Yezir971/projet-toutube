bdec6992009efc842b15bf02e9a49f6e3ea5ef3b
Yezir971 <james_ahmedaly@yahoo.com>
2024-03-06 19:05:57 +0100

    Début du responsive

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,6 +2,34 @@
 
 document.addEventListener('DOMContentLoaded', ()=>{
     AOS.init();
+    // js du menuHamburger 
+        // variable page menu hamburger 
+   
+    let btnMenu = document.querySelector('.menuHamburger');
+    let menu = document.getElementById('menu');
+    let croix1 = document.getElementById('barre1');
+    let croix2 = document.getElementById('barre2');
+
+
+
+    // variable page accueil
+    let sectionPortfolio = document.querySelector(".sectionPortfolio"); 
+
+
+
+    // constante qui va nous permettre de modifier notre css de facon dynamique pour gérer l'ajout d'élément dans notre portfolio 
+
+
+    btnMenu.onclick = function(){/*on récupère les éléments enfants du conteneur hamburger pour réaliser l'animations en css grâce à l'id croix*/
+        menu.classList.toggle('hide');
+        croix1.classList.toggle('croix');
+        croix2.classList.toggle('croix');
+        croix1.classList.toggle('angleBarre1');
+        croix2.classList.toggle('angleBarre2');
+    }
+
+    // fin du js menuHamburger
+
     // crousel importer de codepen créer par Yulia Verbylo, Slider with scaling in the center
     
     const track = document.querySelector(".carousel__track"); //ul
