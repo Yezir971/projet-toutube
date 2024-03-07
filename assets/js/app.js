d91e2aff317d55b6c4c30458e7ebdd4bb90b6632
Yezir971 <james_ahmedaly@yahoo.com>
2024-03-07 23:26:26 +0100

    site v.3

--- assets/js/app.js
+++ assets/js/app.js
@@ -43,7 +43,7 @@ document.addEventListener('DOMContentLoaded', ()=>{
     const imageSrc = 'assets/img/image-projet-youtube/';
     
     // Images are from unsplash
-    let pictures = [imageSrc+"imgRemake3.png",imageSrc+"imgProjet1.jpg", imageSrc+"img3.jpg" ];
+    let pictures = [imageSrc+"img3.jpg" ,imageSrc+"imgRemake3.png",imageSrc+"imgProjet1.jpg" ];
     
     img.src = pictures[0];
     let position = 0;
